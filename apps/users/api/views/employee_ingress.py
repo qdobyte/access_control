aacaf3749146cc26fb49cd270d258c353481d447
Andrés Ramírez <qdobyte@192.168.1.15>
2023-05-12 03:39:03 -0500

    feature(employee_ingress): validate reason for leaving

--- apps/users/api/views/employee_ingress.py
+++ apps/users/api/views/employee_ingress.py
@@ -1,3 +1,5 @@
+from datetime import datetime, time
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -42,17 +44,31 @@ class EmployeeIngressViewSet(viewsets.ModelViewSet):
         Returns:
             Response: The response object with a success or error message.
         """
+        LIMIT_TIME = time(hour=16)
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        if serializer.is_valid():
-            # Check if egress_time is being set
-            if 'egress_time' in request.data and request.data['egress_time'] is not None:
-                # Update is_inside to False
-                request.data['is_inside'] = False
+        serializer = self.get_serializer(instance, data=request.data, partial=partial, many=False)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if egress_time is being set and if employee is leaving before 16:00
+        egress_time = request.data.get('egress_time')
+        if egress_time:
+            egress_time = datetime.strptime(egress_time, '%Y-%m-%d %H:%M:%S.%f')
+            if egress_time.time() < LIMIT_TIME:
+                # Ask for reason of leaving
+                reason = request.data.get('reason_for_leaving')
+                if reason not in ['CITA', 'CALAMIDAD', 'DILIGENCIA']:
+                    return Response({
+                        'message': 'Invalid reason for leaving. Allowed values are "Cita médica", '
+                                   '"calamidad", or "diligencia personal".'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+            request.data['is_inside'] = False
+
+        if serializer.validated_data:
             self.perform_update(serializer)
             return Response({'message': 'EmployeeIngress updated successfully'}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, *args, **kwargs):
         """
